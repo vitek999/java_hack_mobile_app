a786fd773fed719aacdeac806bfd7909582f3967
vitek999 <vitek2012rus@gmail.com>
2019-09-14 18:52:30 +0400

    Add initialization user preferences

--- app/src/main/java/ru/visdom/raiffeisenbusinessad/MainActivity.kt
+++ app/src/main/java/ru/visdom/raiffeisenbusinessad/MainActivity.kt
@@ -2,11 +2,15 @@ package ru.visdom.raiffeisenbusinessad
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import ru.visdom.raiffeisenbusinessad.preferences.UserPreferences
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        // Initialization user preferences
+        UserPreferences.init(applicationContext)
     }
 }
