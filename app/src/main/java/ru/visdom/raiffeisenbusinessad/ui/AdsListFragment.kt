d498744e56ab1c6468bfb2ad73b8cb9efd98d63c
vitek999 <vitek2012rus@gmail.com>
2019-09-14 22:24:46 +0400

    Create AdsListFragment

---
+++ app/src/main/java/ru/visdom/raiffeisenbusinessad/ui/AdsListFragment.kt
@@ -0,0 +1,29 @@
+package ru.visdom.raiffeisenbusinessad.ui
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
+import androidx.fragment.app.Fragment
+import ru.visdom.raiffeisenbusinessad.R
+import ru.visdom.raiffeisenbusinessad.databinding.FragmentAdsLsitBinding
+
+class AdsListFragment : Fragment() {
+
+    private lateinit var binding: FragmentAdsLsitBinding
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        binding = DataBindingUtil.inflate(inflater, R.layout.fragment_ads_lsit, container, false)
+        init()
+        return binding.root
+    }
+
+    private fun init() {
+
+    }
+}
