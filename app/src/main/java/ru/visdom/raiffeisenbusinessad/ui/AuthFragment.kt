b81344811e0bdfe748babce04fc35ce156588aed
vitek999 <vitek2012rus@gmail.com>
2019-09-14 16:56:50 +0400

    Create Auth Fragment

---
+++ app/src/main/java/ru/visdom/raiffeisenbusinessad/ui/AuthFragment.kt
@@ -0,0 +1,28 @@
+package ru.visdom.raiffeisenbusinessad.ui
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
+import androidx.fragment.app.Fragment
+import ru.visdom.raiffeisenbusinessad.R
+import ru.visdom.raiffeisenbusinessad.databinding.FragmentAuthBinding
+
+class AuthFragment : Fragment() {
+    private lateinit var binding: FragmentAuthBinding
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        binding = DataBindingUtil.inflate(inflater, R.layout.fragment_auth, container, false)
+        init()
+        return binding.root
+    }
+
+    private fun init() {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+}
